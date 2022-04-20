211e711d009c9ca5576779ff46423c7b61707b76
moqi <moqimoqidea@gmail.com>
2022-04-20 23:08:02 +0800

    init

---
+++ src/main/java/com/moqi/webflux/MoqiSpringWebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.moqi.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoqiSpringWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoqiSpringWebfluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/moqi/webflux/MoqiSpringWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moqi.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoqiSpringWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
